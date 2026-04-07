a0f330cdae4b471df914533a2d71cee201618fc2
Kian Jay Garcia <kianjaygarcia1234@gmail.com>
2026-04-07 15:55:21 +0800

    Add missing import for Properties

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,5 +1,3 @@
-import org.gradle.api.file.Directory
-import org.gradle.api.tasks.Delete
 import java.util.Properties
 
 pluginManagement {
@@ -27,29 +25,4 @@ plugins {
     id("org.jetbrains.kotlin.android") version "2.1.0" apply false
 }
 
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
-rootProject.name = "flutter_application_1"
 include(":app")
-
-val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
-rootProject.layout.buildDirectory.value(newBuildDir)
-
-subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
-}
-
-subprojects {
-    project.evaluationDependsOn(":app")
-}
-
-tasks.register<Delete>("clean") {
-    delete(rootProject.layout.buildDirectory)
-}
